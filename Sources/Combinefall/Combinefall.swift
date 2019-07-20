567d99bb35401651174bb62cb8aa0bd9f3ea73fd
Marcus Isaksson <me@naknut.email>
2019-07-20 14:12:03 +0200

    add documentation and some refacotring

--- Sources/Combinefall/Combinefall.swift
+++ Sources/Combinefall/Combinefall.swift
@@ -7,18 +7,18 @@ fileprivate enum EndpointComponents {
     case autocomplete
     
     var urlComponents: URLComponents {
-        var baseURLCopy = baseURLComponent
+        var urlComponent = baseURLComponent
         switch self {
-        case .autocomplete: baseURLCopy.path = "/cards/autocomplete"
+        case .autocomplete: urlComponent.path = "/cards/autocomplete"
         }
-        return baseURLCopy
+        return urlComponent
     }
 }
 
-/// Returns a publisher connected to upstream that queries the autocomplete endpoint of Scryfall and publishes a `AutocompleteCatalog`.
-///
-/// - Parameter upstream: A publisher which `Output` must be `String`.
-/// - Parameter scheduler: The `RunLoop` on where to preform the operations. Default is the current `RunLoop`.
+/// Creates a publisher connected to upstream that queries the autocomplete endpoint of Scryfall.
+/// - Parameter upstream: _Required_ A publisher which `Output` must be `String`.
+/// - Parameter scheduler: _Required_ The `Scheduler` on where to preform the operations.
+/// - Returns: A publisher that publishes the results of the query as a `AutocompleteCatalog`
 public func autocompleteCatalogPublisher<U: Publisher, S: Scheduler>(upstream: U, scheduler: S) -> AnyPublisher<AutocompleteCatalog, Never> where U.Output == String, U.Failure == Never {
     upstream
         .filter { $0.count >= 2 }
@@ -38,6 +38,11 @@ public func autocompleteCatalogPublisher<U: Publisher, S: Scheduler>(upstream: U
         .eraseToAnyPublisher()
 }
 
+
+/// Creates a publisher connected to upstream that queries the autocomplete endpoint of Scryfall.
+/// - Parameter upstream: _Required_ A publisher which `Output` must be `String`.
+/// - Parameter scheduler: _Required_ The `Scheduler` on where to preform the operations.
+/// - Returns: A publisher that publishes the results of the query as a `[String]`
 public func autocompletePublisher<U: Publisher, S: Scheduler>(upstream: U, scheduler: S) -> AnyPublisher<[String], Never> where U.Output == String, U.Failure == Never {
     autocompleteCatalogPublisher(upstream: upstream, scheduler: scheduler)
         .map { $0.data }
@@ -45,10 +50,18 @@ public func autocompletePublisher<U: Publisher, S: Scheduler>(upstream: U, sched
 }
 
 public extension Publisher where Self.Output == String, Self.Failure == Never {
+    
+    
+    /// Queries the autocomplete endpoint of Scryfall.
+    /// - Parameter scheduler: _Required_ The scheduler on which this publisher delivers elements
+    /// - Returns: A publisher that publishes the results of the query as a `AutocompleteCatalog`
     func autocompleteList<S: Scheduler>(on scheduler: S) -> AnyPublisher<AutocompleteCatalog, Never> {
         autocompleteCatalogPublisher(upstream: self, scheduler: scheduler)
     }
     
+    /// Queries the autocomplete endpoint of Scryfall.
+    /// - Parameter scheduler: _Required_ The scheduler on which this publisher delivers elements
+    /// - Returns: A publisher that publishes the results of the query as a `[String]`
     func autocomplete<S: Scheduler>(on scheduler: S) -> AnyPublisher<[String], Never> {
         autocompletePublisher(upstream: self, scheduler: scheduler)
     }
