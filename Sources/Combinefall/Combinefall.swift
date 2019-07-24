614c14ddc878f38b1da38a9a3f953a39ee897877
Marcus Isaksson <me@naknut.email>
2019-07-24 02:06:37 +0200

    Can now inject remote publisher for better testing

--- Sources/Combinefall/Combinefall.swift
+++ Sources/Combinefall/Combinefall.swift
@@ -15,6 +15,24 @@ fileprivate enum EndpointComponents {
     }
 }
 
+// Used internaly to inject remote publisher for testing.
+func _autocompleteCatalogPublisher<U: Publisher, R: Publisher, S: Scheduler>(upstream: U, remotePublisherFactory: @escaping (URL) -> R, scheduler: S) -> AnyPublisher<AutocompleteCatalog, Never> where U.Output == String, U.Failure == Never, R.Output == URLSession.DataTaskPublisher.Output, R.Failure == Never {
+    upstream
+        .filter { $0.count >= 2 }
+        .debounce(for: .milliseconds(100), scheduler: scheduler)
+        .removeDuplicates()
+        .compactMap { searchTerm in
+            var autoCompleteComponents = EndpointComponents.autocomplete.urlComponents
+            autoCompleteComponents.queryItems = [URLQueryItem(name: "q", value: searchTerm)]
+            return autoCompleteComponents.url
+        }
+        .flatMap { (url: URL) in remotePublisherFactory(url) }
+        .map { $0.data }
+        .decode(type: AutocompleteCatalog.self, decoder: JSONDecoder())
+        .replaceError(with: AutocompleteCatalog(totalValues: 0, data: []))
+        .eraseToAnyPublisher()
+}
+
 /// Creates a publisher connected to upstream that queries the autocomplete endpoint of Scryfall.
 ///
 /// This function is designed for creating assistive UI elements that allow users to free-type card names.
@@ -28,22 +46,7 @@ fileprivate enum EndpointComponents {
 /// - Parameter scheduler: _Required_ The `Scheduler` on where to preform the operations.
 /// - Returns: A publisher that publishes a `AutocompleteCatalog` containing up to 20 full English card names that could be autocompletions of the given `upstream` published element.
 public func autocompleteCatalogPublisher<U: Publisher, S: Scheduler>(upstream: U, scheduler: S) -> AnyPublisher<AutocompleteCatalog, Never> where U.Output == String, U.Failure == Never {
-    upstream
-        .filter { $0.count >= 2 }
-        .debounce(for: .milliseconds(100), scheduler: scheduler)
-        .removeDuplicates()
-        .compactMap { searchTerm in
-            var autoCompleteComponents = EndpointComponents.autocomplete.urlComponents
-            autoCompleteComponents.queryItems = [URLQueryItem(name: "q", value: searchTerm)]
-            return autoCompleteComponents.url
-        }
-        .flatMap { (url: URL) in
-            URLSession.shared.dataTaskPublisher(for: url).assertNoFailure()
-        }
-        .map { $0.data }
-        .decode(type: AutocompleteCatalog.self, decoder: JSONDecoder())
-        .replaceError(with: AutocompleteCatalog(totalValues: 0, data: []))
-        .eraseToAnyPublisher()
+    _autocompleteCatalogPublisher(upstream: upstream, remotePublisherFactory: { URLSession.shared.dataTaskPublisher(for: $0).assertNoFailure() }, scheduler: scheduler)
 }
 
 
@@ -66,7 +69,6 @@ public func autocompletePublisher<U: Publisher, S: Scheduler>(upstream: U, sched
 }
 
 public extension Publisher where Self.Output == String, Self.Failure == Never {
-    
     
     /// Queries the autocomplete endpoint of Scryfall.
     /// This function is designed for creating assistive UI elements that allow users to free-type card names.
--- Tests/CombinefallTests/CombinefallTests.swift
+++ Tests/CombinefallTests/CombinefallTests.swift
@@ -1,5 +1,32 @@
 import XCTest
+import Combine
 @testable import Combinefall
+
+struct URLSessionMockPublisher: Publisher {
+    typealias Output = URLSession.DataTaskPublisher.Output
+    typealias Failure = Never
+    
+    func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        subscriber.receive(subscription: URLSessionMockPublisherSubscription(subscriber: subscriber))
+    }
+}
+
+class URLSessionMockPublisherSubscription<S: Subscriber>: Subscription where S.Input == URLSessionMockPublisher.Output {
+    var subscriber: S?
+    let testData = try! JSONEncoder().encode(AutocompleteCatalog(totalValues: 1, data: ["Jace"]))
+    
+    init(subscriber: S) {
+        self.subscriber = subscriber
+    }
+    
+    func request(_ demand: Subscribers.Demand) {
+        _ = subscriber?.receive((testData, URLResponse()))
+    }
+    
+    func cancel() {
+        subscriber = nil
+    }
+}
 
 final class CombinefallTests: XCTestCase {
     func testCatalog() {
@@ -15,8 +42,24 @@ final class CombinefallTests: XCTestCase {
         }
         XCTAssert(catalog.totalValues == 0)
     }
+    
+    @Published var testUpstream: String = ""
+    
+    func testAutocomplete() {
+        let expectation = XCTestExpectation(description: "Let publisher publish")
+        _ = _autocompleteCatalogPublisher(upstream: $testUpstream, remotePublisherFactory: { (_: URL) in URLSessionMockPublisher() }, scheduler: RunLoop.current)
+            .sink { catalog in
+                XCTAssert(catalog.totalValues == 1)
+                XCTAssert(catalog.data.count == 1)
+                XCTAssert(catalog.data.first == "Jace")
+                expectation.fulfill()
+            }
+        testUpstream = "Foo"
+        wait(for: [expectation], timeout: 10.0)
+    }
 
     static var allTests = [
         ("testCatalog", testCatalog),
+        ("testAutocomplete", testAutocomplete),
     ]
 }
