374ee3a550379ab4b1c3f4be848e57db64ba1fbe
Marcus Isaksson <me@naknut.email>
2019-08-06 17:20:29 +0200

    add documentation to card

--- Sources/Combinefall/Card.swift
+++ Sources/Combinefall/Card.swift
@@ -1,6 +1,16 @@
 import Foundation
 
+///Represent individual _Magic: The Gathering_ cards that players
+///could obtain and add to their collection (with a few minor exceptions).
 public struct Card: Decodable {
+
+    //MARK: - Core Card Properties
+    
+    /// A language that a `Card` can be printed in.
+    ///
+    ///Scryfall archives Magic cards in 17 languages (including some unofficial languages).
+    ///
+    ///Our support for multiple languages in older sets is limited. We are expanding this data slowly over time.
     public enum Language: String, Decodable {
         case english = "en"
         case spanish = "es"
@@ -21,19 +31,52 @@ public struct Card: Decodable {
         case phyrexian = "px"
     }
 
+    ///The Arena ID, if any. A large percentage of cards are not available on Arena and if not this will be `nil`.
     let arenaIdentifier: Int?
+    
+    ///A unique ID for this `Card` in Scryfall’s database.
     let identifier: UUID
+    
+    ///The `Language` for this printing.
     let language: Language
+    
+    ///The Magic Online ID (also known as the Catalog ID), if any.
+    ///
+    ///A large percentage of cards are not available on Magic Online and if not this will be `nil`.
     let magicOnlineIdentifier: Int?
+    
+    ///The foil Magic Online ID (also known as the Catalog ID), if any.
+    ///
+    ///A large percentage of cards are not available on Magic Online and if not this will be `nil`.
     let magicOnlineFoilIdentifier: Int?
+    
+    ///The multiverse IDs on Gatherer, if any, as an array of integers.
+    ///
+    ///Scryfall includes many promo cards, tokens, and other esoteric objects that do not have these identifiers.
     let multiverseIdentifier: [Int]?
+    
+    ///The ID on TCGplayer’s API, also known as the productId.
     let tcgPlayerIdentifier: Int
+    
+    ///A unique ID for oracle identity.
+    ///
+    ///This value is consistent across reprinted card editions,
+    ///and unique among different cards with the same name (tokens, Unstable variants, etc).
     let oracleIdentifier: UUID
+    
+    ///A link to where you can begin paginating all re/prints for this card on Scryfall’s API.
     let printsSearchUrl: URL
+    
+    ///A link to the rulings list for this `Card` on Scryfall’s API.
     let rulingsUrl: URL
+    
+    ///A link to the permapage for this `Card` on Scryfall’s website.
     let scryfallUrl: URL
+    
+    ///A link to this `Card` on Scryfall’s API.
     let url: URL
 
+    //MARK: - Decodeable
     enum CodingKeys: String, CodingKey {
         case arenaIdentifier = "arena_id"
         case identifier
