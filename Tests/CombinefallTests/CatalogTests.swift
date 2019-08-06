561c00e7007afa2d466041f7cb66b973cf15b0fe
Marcus Isaksson <me@naknut.email>
2019-08-06 15:45:22 +0200

    add tests for catalog

---
+++ Tests/CombinefallTests/CatalogTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import Combinefall
+
+final class CatalogTests: XCTestCase {
+    
+    func testTotalValuesCodingKey() {
+        XCTAssert(Catalog<String>.CodingKeys(rawValue: "total_values") == .totalValues, "CodingKey is not of the right case")
+    }
+}
