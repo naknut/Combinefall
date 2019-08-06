aa3aa077ad84caf8278f4d3808e79cb6e8894a0d
Marcus Isaksson <me@naknut.email>
2019-08-06 17:00:50 +0200

    fix documentation on Catalog

--- Sources/Combinefall/Catalog.swift
+++ Sources/Combinefall/Catalog.swift
@@ -1,6 +1,6 @@
-/// A Catalog object contains an array of Magic datapoints (words, card values, etc).
+/// Contains an array of Magic datapoints (words, card values, etc).
 ///
-/// Catalog objects are provided by the API as aids for building other Magic software and
+/// `Catalog` is provided by `Combinefall` as a aid for building other Magic software and
 /// understanding possible values for a field on Card objects.
 public struct Catalog<T: Decodable>: Decodable {
 
