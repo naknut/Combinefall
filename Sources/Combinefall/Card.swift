2ce87396cc3dc4cc9e1595fa05996e8f05500238
Marcus Isaksson <me@naknut.email>
2019-08-07 11:56:42 +0200

    made Card properties public

--- Sources/Combinefall/Card.swift
+++ Sources/Combinefall/Card.swift
@@ -32,49 +32,49 @@ public struct Card: Decodable {
     }
 
     ///The Arena ID, if any. A large percentage of cards are not available on Arena and if not this will be `nil`.
-    let arenaIdentifier: Int?
+    public let arenaIdentifier: Int?
     
     ///A unique ID for this `Card` in Scryfall’s database.
-    let identifier: UUID
+    public let identifier: UUID
     
     ///The `Language` for this printing.
-    let language: Language
+    public let language: Language
     
     ///The Magic Online ID (also known as the Catalog ID), if any.
     ///
     ///A large percentage of cards are not available on Magic Online and if not this will be `nil`.
-    let magicOnlineIdentifier: Int?
+    public let magicOnlineIdentifier: Int?
     
     ///The foil Magic Online ID (also known as the Catalog ID), if any.
     ///
     ///A large percentage of cards are not available on Magic Online and if not this will be `nil`.
-    let magicOnlineFoilIdentifier: Int?
+    public let magicOnlineFoilIdentifier: Int?
     
     ///The multiverse IDs on Gatherer, if any, as an array of integers.
     ///
     ///Scryfall includes many promo cards, tokens, and other esoteric objects that do not have these identifiers.
-    let multiverseIdentifier: [Int]?
+    public let multiverseIdentifier: [Int]?
     
     ///The ID on TCGplayer’s API, also known as the productId.
-    let tcgPlayerIdentifier: Int
+    public let tcgPlayerIdentifier: Int
     
     ///A unique ID for oracle identity.
     ///
     ///This value is consistent across reprinted card editions,
     ///and unique among different cards with the same name (tokens, Unstable variants, etc).
-    let oracleIdentifier: UUID
+    public let oracleIdentifier: UUID
     
     ///A link to where you can begin paginating all re/prints for this card on Scryfall’s API.
-    let printsSearchUrl: URL
+    public let printsSearchUrl: URL
     
     ///A link to the rulings list for this `Card` on Scryfall’s API.
-    let rulingsUrl: URL
+    public let rulingsUrl: URL
     
     ///A link to the permapage for this `Card` on Scryfall’s website.
-    let scryfallUrl: URL
+    public let scryfallUrl: URL
     
     ///A link to this `Card` on Scryfall’s API.
-    let url: URL
+    public let url: URL
 
     //MARK: - Decodeable
     enum CodingKeys: String, CodingKey {
