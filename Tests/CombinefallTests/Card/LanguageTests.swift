48117e0f7eebc6f963d310ae9c4920020eea7d68
Marcus Isaksson <me@naknut.email>
2019-08-08 14:27:17 +0200

    clean up in language tests

--- Tests/CombinefallTests/Card/LanguageTests.swift
+++ Tests/CombinefallTests/Card/LanguageTests.swift
@@ -2,119 +2,119 @@ import XCTest
 @testable import Combinefall
 
 final class LanguageTests: XCTestCase {
-    func testLanguageEnglishRawValue() {
+    func testEnglishRawValue() {
         XCTAssert(
             Card.Language(rawValue: "en") == .english,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageSpanishRawValue() {
+    func testSpanishRawValue() {
         XCTAssert(
             Card.Language(rawValue: "es") == .spanish,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageFrenchRawValue() {
+    func testFrenchRawValue() {
         XCTAssert(
             Card.Language(rawValue: "fr") == .french,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageGermanRawValue() {
+    func testGermanRawValue() {
         XCTAssert(
             Card.Language(rawValue: "de") == .german,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageItalianRawValue() {
+    func testItalianRawValue() {
         XCTAssert(
             Card.Language(rawValue: "it") == .italian,
             "Language is not of the right case"
         )
     }
 
-    func testLanguagePortugueseRawValue() {
+    func testPortugueseRawValue() {
         XCTAssert(
             Card.Language(rawValue: "pt") == .portuguese,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageJapaneseRawValue() {
+    func testJapaneseRawValue() {
         XCTAssert(
             Card.Language(rawValue: "ja") == .japanese,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageKoreanRawValue() {
+    func testKoreanRawValue() {
         XCTAssert(
             Card.Language(rawValue: "ko") == .korean,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageRussianRawValue() {
+    func testRussianRawValue() {
         XCTAssert(
             Card.Language(rawValue: "ru") == .russian,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageSimplifiedChineseRawValue() {
+    func testSimplifiedChineseRawValue() {
         XCTAssert(
             Card.Language(rawValue: "zhs") == .simplifiedChinese,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageTraditionalChineseRawValue() {
+    func testTraditionalChineseRawValue() {
         XCTAssert(
             Card.Language(rawValue: "zht") == .traditionalChinese,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageHebrewRawValue() {
+    func testHebrewRawValue() {
         XCTAssert(
             Card.Language(rawValue: "he") == .hebrew,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageLatinRawValue() {
+    func testLatinRawValue() {
         XCTAssert(
             Card.Language(rawValue: "la") == .latin,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageAncientGreekRawValue() {
+    func testAncientGreekRawValue() {
         XCTAssert(
             Card.Language(rawValue: "grc") == .ancientGreek,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageArabicRawValue() {
+    func testArabicRawValue() {
         XCTAssert(
             Card.Language(rawValue: "ar") == .arabic,
             "Language is not of the right case"
         )
     }
 
-    func testLanguageSanskritRawValue() {
+    func testSanskritRawValue() {
         XCTAssert(
             Card.Language(rawValue: "sa") == .sanskrit,
             "Language is not of the right case"
         )
     }
 
-    func testLanguagePhyrexianRawValue() {
+    func testPhyrexianRawValue() {
         XCTAssert(
             Card.Language(rawValue: "px") == .phyrexian,
             "Language is not of the right case"
@@ -122,22 +122,22 @@ final class LanguageTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testLanguageEnglishRawValue", testLanguageEnglishRawValue),
-        ("testLanguageSpanishRawValue", testLanguageSpanishRawValue),
-        ("testLanguageFrenchRawValue", testLanguageFrenchRawValue),
-        ("testLanguageGermanRawValue", testLanguageGermanRawValue),
-        ("testLanguageItalianRawValue", testLanguageItalianRawValue),
-        ("testLanguagePortugueseRawValue", testLanguagePortugueseRawValue),
-        ("testLanguageJapaneseRawValue", testLanguageJapaneseRawValue),
-        ("testLanguageKoreanRawValue", testLanguageKoreanRawValue),
-        ("testLanguageRussianRawValue", testLanguageRussianRawValue),
-        ("testLanguageSimplifiedChineseRawValue", testLanguageSimplifiedChineseRawValue),
-        ("testLanguageTraditionalChineseRawValue", testLanguageTraditionalChineseRawValue),
-        ("testLanguageHebrewRawValue", testLanguageHebrewRawValue),
-        ("testLanguageLatinRawValue", testLanguageLatinRawValue),
-        ("testLanguageAncientGreekRawValue", testLanguageAncientGreekRawValue),
-        ("testLanguageArabicRawValue", testLanguageArabicRawValue),
-        ("testLanguageSanskritRawValue", testLanguageSanskritRawValue),
-        ("testLanguagePhyrexianRawValue", testLanguagePhyrexianRawValue)
+        ("testEnglishRawValue", testEnglishRawValue),
+        ("testSpanishRawValue", testSpanishRawValue),
+        ("testFrenchRawValue", testFrenchRawValue),
+        ("testGermanRawValue", testGermanRawValue),
+        ("testItalianRawValue", testItalianRawValue),
+        ("testPortugueseRawValue", testPortugueseRawValue),
+        ("testJapaneseRawValue", testJapaneseRawValue),
+        ("testKoreanRawValue", testKoreanRawValue),
+        ("testRussianRawValue", testRussianRawValue),
+        ("testSimplifiedChineseRawValue", testSimplifiedChineseRawValue),
+        ("testTraditionalChineseRawValue", testTraditionalChineseRawValue),
+        ("testHebrewRawValue", testHebrewRawValue),
+        ("testLatinRawValue", testLatinRawValue),
+        ("testAncientGreekRawValue", testAncientGreekRawValue),
+        ("testArabicRawValue", testArabicRawValue),
+        ("testSanskritRawValue", testSanskritRawValue),
+        ("testPhyrexianRawValue", testPhyrexianRawValue)
     ]
 }
