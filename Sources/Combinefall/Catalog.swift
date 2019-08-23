d92a0a09e9dc698c0fdea5f4d5e99b1e1a882d66
Marcus Isaksson <me@naknut.email>
2019-08-23 14:03:17 +0200

    🐛 Access modifiers for Catalog
    Changed so some properties in Catalog is public. They should always have been public.

--- Sources/Combinefall/Catalog.swift
+++ Sources/Combinefall/Catalog.swift
@@ -6,10 +6,10 @@ public struct Catalog<T: Decodable>: ScryfallModel {
     let object: String
 
     /// The number of items in the `data` array.
-    let totalValues: Int
+    public let totalValues: Int
 
     /// An array of datapoints.
-    let data: [T]
+    public let data: [T]
 
     enum CodingKeys: String, CodingKey {
         case object, totalValues = "total_values", data
